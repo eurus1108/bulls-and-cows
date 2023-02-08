f38ff28bb12e3b3fee8d7c3422fb7841501f57c4
Eurus <eurus0601@gmail.com>
2023-02-08 22:16:23 +0800

    copying and printing example

---
+++ Stage 1/Main.java
@@ -0,0 +1,21 @@
+/**
+ * Main
+ */
+public class Main {
+    public static void main(String[] args) {
+        String example = """
+                The secret code is prepared: ****.
+
+                Turn 1. Answer:
+                1234
+                Grade: None.
+
+                Turn 2. Answer:
+                9876
+                Grade: 4 bulls.
+                Congrats! The secret code is 9876.
+                    """;
+
+        System.out.println();
+    }
+}
