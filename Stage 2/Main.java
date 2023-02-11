780a5b15b02a98612f8cbe62ff4f67897d9267a3
Eurus <eurus0601@gmail.com>
2023-02-11 08:35:20 +0800

    String Matching

--- Stage 2/Main.java
+++ Stage 2/Main.java
@@ -1,8 +1,43 @@
+import java.util.Scanner;
+
 /**
  * Main
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        final String secretCode = "9305";
+        int bulls = 0;
+        int cows = 0;
+
+        System.out.print("> ");
+        String guessCode = scanner.nextLine();
+
+        for (int i = 0; i < secretCode.length(); i++) {
+            if (guessCode.charAt(i) == secretCode.charAt(i)) {
+                bulls++;
+            }
+
+            for (int j = 0; j < secretCode.length(); j++) {
+                if (guessCode.charAt(j) == secretCode.charAt(i) && !(guessCode.charAt(i) == secretCode.charAt(i))) {
+                    cows++;
+                }
+            }
+        }
+
+        String grade = "";
+
+        if (bulls > 0 && cows > 0) {
+            grade = bulls + " bulls(s) and " + cows + " cows(s)";
+        } else if (bulls > 0) {
+            grade = bulls + " bulls(s)";
+        } else if (cows > 0) {
+            grade = cows + " cow(s)";
+        } else {
+            grade = "None";
+        }
+
+        System.out.println("Grade: " + grade + ". The secret code is " + secretCode + ".");
     }
 }
