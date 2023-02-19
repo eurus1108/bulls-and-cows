e8352756c4eab5d9b202909ed28dcbc03e434c44
Eurus <eurus0601@gmail.com>
2023-02-19 22:34:26 +0800

    Generating random numbers

--- Stage 3/Main.java
+++ Stage 3/Main.java
@@ -7,22 +7,39 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // System.out.print("> ");
-        long psuedoRandomNumber = System.nanoTime();
+        System.out.print("> ");
+        int stringLength = scanner.nextInt();
 
-        StringBuilder code = new StringBuilder(Long.toString(psuedoRandomNumber));
-        System.out.println("Original String: " + code);
+        if (stringLength > 10) {
+            System.out.println(
+                    "Error: can't generate a secret number with a length of 11 because there aren't enough unique digits.");
+            return;
+        }
 
-        code.reverse();
-        System.out.println("Reverse String: " + code);
+        String initialCode = randomNumber();
+        String secretCode = "";
 
-        StringBuilder noDuplicate = new StringBuilder(removeDuplicate(code));
+        for (int i = 0; i < stringLength; i++) {
+            secretCode += initialCode.charAt(i);
+        }
 
-        System.out.println("No Duplicate: " + noDuplicate);
+        System.out.println("The random secret number is " + secretCode + ".");
     }
 
-    public static StringBuilder removeDuplicate(StringBuilder string) {
-        StringBuilder code = new StringBuilder();
+    public static String randomNumber() {
+        long psuedoRandomNumber = System.nanoTime();
+
+        // I use StringBuilder because I want to use the reverse() function built in.
+        StringBuilder reverseNumber = new StringBuilder(Long.toString(psuedoRandomNumber)).reverse();
+
+        String filteredNumber = filterDuplicate(reverseNumber);
+        String code = codeGenerator(filteredNumber);
+
+        return code;
+    }
+
+    public static String filterDuplicate(StringBuilder string) {
+        StringBuilder filteredNumber = new StringBuilder();
 
         for (int i = 0; i < string.length(); i++) {
             int j;
@@ -33,11 +50,23 @@ public class Main {
             }
 
             if (i == j) {
-                code.append(string.charAt(i));
+                filteredNumber.append(string.charAt(i));
             }
         }
 
-        code.deleteCharAt(0);
+        filteredNumber.deleteCharAt(0);
+
+        return filteredNumber.toString();
+    }
+
+    public static String codeGenerator(String code) {
+        String[] listOfNumbers = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+        for (int i = 0; i < listOfNumbers.length; i++) {
+            if (!code.contains(listOfNumbers[i])) {
+                code += listOfNumbers[i];
+            }
+        }
 
         return code;
     }
